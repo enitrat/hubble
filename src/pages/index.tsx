534f77ab1aebce86399ba00e8367ceac3f43561e
neeeekitos <nikita.a.terekhov@gmail.com>
2022-07-19 07:55:12 +0200

    new ui

---
+++ src/pages/index.tsx
@@ -0,0 +1,27 @@
+import { Box } from "@chakra-ui/react";
+import dynamic from "next/dynamic";
+
+import CTASection from "components/samples/CTASection";
+import SomeText from "components/samples/SomeText";
+import { MintTokens, Transactions } from "components/wallet";
+
+const Home = () => {
+  // Create a psuedo component using dynamic import that will only be imported client-side
+  const Chart = dynamic(() => import("../components/charts/Chart"), {
+    ssr: false,
+  });
+
+  return (
+    <Box mb={8} w="full" h="full" d="flex" flexDirection="column">
+      <SomeText />
+      <Box flex="1 1 auto">
+        <Transactions />
+        <MintTokens />
+      </Box>
+      <CTASection />
+      <Chart id="chartdiv" />
+    </Box>
+  );
+};
+
+export default Home;
